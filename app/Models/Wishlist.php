5b1b93dcabbbfe0143d2e431e02438c956b7f610
RafifMuh <rafifmoh09@gmail.com>
2026-05-21 19:02:21 +0700

    feat: add Soft Delete at wallet, wishlist, and Transaction

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -4,16 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Wishlist extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
         'name',
-        'target_amount', // <-- Wajib ditambahkan
-        'notes',         // <-- Wajib ditambahkan
+        'target_amount', 
+        'notes',       
         'status',
     ];
 
